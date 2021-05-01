6bb53977adf8d5a23740ca73cd55141caea59ec3
Ruslan <ruslanehg@gmail.com>
2021-05-02 00:39:23 +0600

    16 did

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    Hello medium clone!
+    <mcv-topbar/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import McvTopbar from '@/components/Topbar'
+
+export default {
+  name: 'McvApp',
+  components: {
+    McvTopbar
+  }
+}
+</script>       
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://conduit.productionready.io/api'
+//axios.defaults.baseURL = 'https://conduit.productionready.io/api'
+axios.defaults.baseURL = 'http://localhost:3000/api'
 
 export default axios
